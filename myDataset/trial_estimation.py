899b3b250a232de8b902492d755d188621e52e88
Mina Makary <mina.makary@live.com>
2023-04-09 21:36:22 +0400

    Test 1 (red and blue classifier)
    modified code to start reading when opencv is launched.
    Data is being fed and trained by Kneighborsclassifier

--- myDataset/trial_estimation.py
+++ myDataset/trial_estimation.py
@@ -8,17 +8,19 @@ from sklearn.metrics import accuracy_score
 # Load the dataset
 data = []
 labels = []
-for i in range(6): #why 6 though ??
-    for j in ['blue_0_']:
-        for k in range(1, 20):
-            img = cv2.imread(f"myDataset/blue_0/{j}{k}.jpg")
-            img = cv2.resize(img, (224, 224))
+
+for color in ['blue_0', 'red_0',]:
+    folder_path = f"myDataset/{color}"
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg"):
+            img_path = os.path.join(folder_path, filename)
+            img = cv2.imread(img_path)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
             hist = cv2.calcHist([img], [1, 2], None, [8, 8], [0, 256, 0, 256])
             
             hist = cv2.normalize(hist, hist).flatten()
             data.append(hist)
-            labels.append(j[:-1])
+            labels.append(color)
 
 # Split the dataset
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2)
