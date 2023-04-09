6c8ff3f40e1cd7d4163ec33b9a0f32819bdf3790
Mina Makary <mina.makary@live.com>
2023-04-10 02:08:29 +0400

    adding images different shapes

--- myDataset/trial_estimation.py
+++ myDataset/trial_estimation.py
@@ -5,6 +5,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import tensorflow as tf
+
+
+
 # Load the dataset
 data = []
 labels = []
